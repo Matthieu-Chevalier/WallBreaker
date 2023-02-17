f52a97d33baa5fb39ef2f0861dc195f1435d5fe5
matthieu-chevalier <matthieu.chevalier.pro@gmail.com>
2023-02-17 18:36:52 +0100

    Initial model: Balle

---
+++ Objects/Objets/Balle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Objects.Objets
+{
+    public class Balle
+    {
+        public int BalleX { get; set; }
+        public int BalleY { get; set; }
+        public int BalleDX { get; set;}
+        public int BalleDY { get; set;}
+        public int BalleSize { get; set;}
+    }
+    
+}
