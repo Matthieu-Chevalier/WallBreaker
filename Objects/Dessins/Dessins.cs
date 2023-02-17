5e05740b8ad1e52c9cebc23907bbf1b9249bedc0
matthieu-chevalier <matthieu.chevalier.pro@gmail.com>
2023-02-17 15:34:54 +0100

    Ajoutez des fichiers projet.

---
+++ Objects/Dessins/Dessins.cs
@@ -0,0 +1,24 @@
+﻿using Objets;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dessins
+{
+    public class Dessins
+    {
+        public Graphics ModeleGraphique { get; set; }
+        public Dessins(Graphics modeleGraphique)
+        {
+            ModeleGraphique = modeleGraphique;
+        }
+
+        public void DessinerRaquette(Raquette raquette)
+        {
+            ModeleGraphique.FillRectangle(Brushes.Black, raquette.PositionX, raquette.PositionY, raquette.Largeur, raquette.Hauteur);
+        }
+    }
+}
---
+++ Objects/Objets/Raquette.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Objets
+{
+    public class Raquette
+    {
+        public Raquette(int positionX, int positionY, int hauteur, int largeur)
+        {
+            PositionX = positionX;
+            PositionY = positionY;
+            Hauteur = hauteur;
+            Largeur = largeur;
+        }
+
+        public int PositionX { get; set; }
+        public int PositionY { get;  } // On peut envisager plus tard de modifier la hauteur de la raquette suite à un Malus, mais pas pour l'instant
+        public int Hauteur { get;  } // La hauteur de la raquette ne changera jamais
+        public int Largeur { get; set; }
+
+         
+        public void DeplacerRaquette(int vitesse)
+        {
+            PositionX += vitesse;
+        }
+        
+
+    }
+}
